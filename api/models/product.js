07b4f0e77a43ddceca549d595991be410c4d44fe
pritamkamble <7276279026.pk@gmail.com>
2019-06-27 16:47:05 +0530

    12th video completed

--- api/models/product.js
+++ api/models/product.js
@@ -2,15 +2,9 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name:{
-        type: String,
-        required: true,
-        validate: /[^A-Za-z0-9]+/g
-    },
-    price: {
-        type: Number,
-        required: true
-    }
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    productImage: { type: String, required: true}
 });
 
 module.exports = mongoose.model('Product', productSchema);
--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+const userRoutes = require('./api/routes/user');
 
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
-
-
-mongoose.connect('mongodb+srv://node-shop:node-shop@node-shop-rest-slxeu.mongodb.net/test?retryWrites=true&w=majority');
-
+mongoose.connect('mongodb+srv://node-shop:node-shop@node-shop-rest-slxeu.mongodb.net/test?retryWrites=true&w=majority',
+                   { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
+app.use('/uploads',express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -31,6 +34,7 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
